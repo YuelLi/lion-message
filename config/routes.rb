be76003a634e37a0e6fa295ba21dc53ab0d4da5d
Yu Li <yuelli1997@gmail.com>
2021-03-14 15:29:53 +0800

    update controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#logout'
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
