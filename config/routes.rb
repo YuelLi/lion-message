5914c0896424fb9f46c4cd475c78e291f92a0272
Yu Li <yuelli1997@gmail.com>
2021-03-14 15:49:50 +0800

    update erbs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
+  get '/', to: 'sessions#welcome'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#logout'
