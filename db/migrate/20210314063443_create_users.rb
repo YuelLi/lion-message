fb08d39826e9bdb77a542d3f44f0ae06f199f8ad
xyk <yx2622@columbia.edu>
2021-03-14 15:16:10 +0800

    model

--- db/migrate/20210314063443_create_users.rb
+++ db/migrate/20210314063443_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-
+      t.string :username
+      t.string :password
+      t.string :role
       t.timestamps
     end
   end
