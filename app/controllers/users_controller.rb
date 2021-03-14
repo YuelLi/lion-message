dd3274027326f4338a95bb590447e2a0a4b00909
Yu Li <yuelli1997@gmail.com>
2021-03-14 16:06:28 +0800

    update erbs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,9 @@ skip_before_action :authorized, only: [:new, :create]
   end
 
   def create
-    @user = User.create(params.require(:user).permit(:username,        
-    :password))
+    @user = User.create(params.require(:user).permit(:username,:password))
+    @user.role= 'student'
+    @user.save
     session[:user_id] = @user.id
     redirect_to '/welcome'
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2021_03_14_070025) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
