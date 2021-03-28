77c474fe970db2011b64837823b30e612ecbae66
Yu Li <yuelli1997@gmail.com>
2021-03-28 22:50:55 +0800

    Add default seeds and update view

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,40 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-users = [{:username => 'yuankai', :password => '123456', :role => "student", :department => "Computer Science"}]
+users = [{
+          :username => 'yuankai', 
+          :password => '123456', 
+          :role => "student", 
+          :department => "Computer Science"
+          },
+          {
+          :username => 'zhangsan',
+          :password => '123456',
+          :role => 'student',
+          :department => 'Electrical Engineering'
+          },
+          {
+            :username => 'cs1',
+            :password => '123456',
+            :role => 'faculty',
+            :department => 'Computer Science'
+          },
+          {
+            :username => 'ee1',
+            :password => '123456',
+            :role => 'faculty',
+            :department => 'Electrical Engineering'
+          }
+        ]
 
 users.each do |user|
   User.create!(user)
 end
 
-departments = [{:name => "Computer Science"},{:name => "Electrical Engineering"},{:name => "Columbia Finance"},{:name => "ISSO"}]
-
-users.each do |user|
-  User.create!(user)
-end
+departments = [{:name => "Computer Science"},
+               {:name => "Electrical Engineering"},
+               {:name => "Columbia Finance"},
+               {:name => "ISSO"}]
 
 departments.each do |department|
   Department.create!(department)
