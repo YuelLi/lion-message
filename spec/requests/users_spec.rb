7ff7a9ac3a41d7f194c7c18d58681dfed77ba49a
Yu Li <yuelli1997@gmail.com>
2021-03-29 16:54:21 +0800

    solved pending rspec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,8 +12,9 @@ RSpec.describe "Users", type: :request do
     it "create a new user account and store it in database" do
       username = "testusername"
       password = "testpassword"
+      email = "test@columbia.edu"
       previous_user_count = User.count()
-      post "/users/create", params: {username: username, password:password}
+      post "/users/create", params: {username: username, password:password, email:email}
       expect(User.count()).to eq(previous_user_count+1)
       expect(response).to redirect_to("/welcome")
     end
